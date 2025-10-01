8d11bda9e4ad45d4adfdd29b7536674faee5587f
Keshav Sharma <sharmakeshav802@gmail.com>
2025-10-01 21:44:49 +0530

    added register page abd register dto for idenytity

---
+++ ContractsManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using ContactsManager.Core.DTO;
+using CURDDEMO.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractsManager.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            return RedirectToAction(nameof(PersonsController.Index), "Person");
+        }
+
+    }
+}
